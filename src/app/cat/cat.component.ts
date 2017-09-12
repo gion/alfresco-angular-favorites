0a465699a7292e0e38d650d445463eb63dfd95dd
gion <bogdan.gradinariu@thinslices.com>
2017-09-12 21:49:04 +0300

    Update the App with a cat :D

---
+++ src/app/cat/cat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cat',
+  templateUrl: './cat.component.html',
+  styleUrls: ['./cat.component.css'],
+})
+export class CatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cat/index.ts
@@ -0,0 +1,1 @@
+export { CatComponent } from './cat.component'
